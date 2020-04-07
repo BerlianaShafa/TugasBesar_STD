4caa465c3a646b967f255559f843bc9e3d428793
BerlianaShafa <59946995+BerlianaShafa@users.noreply.github.com>
2020-04-07 21:48:29 +0700

    update list
    list header child dan parent

--- listChild.cpp
+++ listChild.cpp
@@ -1,1 +1,51 @@
+#ifndef LISTCHILD_H_INCLUDED
+#define LISTCHILD_H_INCLUDED
+#include <iostream>
+#define next(P) P->next
+#define prev(P) P->prev
+#define info(P) P->info
+#define firstChild(L) L.firstChild
+
+// Double Circular
+
+using namespace std;
+
+struct infoChild
+{
+    int idCustomer;
+    string jenisBusana;
+    string ukuranBusana;
+    int jumlahBusana;
+};
+
+typedef struct elmlistChild *addressChild;
+
+struct elmlistChild
+{
+    infoChild info;
+    addressChild next;
+    addressChild prev;
+};
+
+struct listChild
+{
+    addressChild firstChild;
+};
+
+void createListChild(listChild &L);
+addressChild allocateChild(infochild x);
+void insertFirstChild(listChild &L, addressChild P);
+void insertAfterChild(listChild &L, addressChild prec, addressChild P);
+void insertLastChild(listChild &L, addressChild P);
+void deleteFirstChild(listChild &L, addressChild &P);
+void deleteAfterChild(listChild &L, addressChild prec, addressChild &P);
+void deleteLastChild(listChild &L, addressChild &P);
+void printinfoChild(listChild L);
+addressChild searchElmChild(listChild &L, int noCust);
+
+#endif // LISTCHILD_H_INCLUDED
+
+
+
+
 
--- listParent.cpp
+++ listParent.cpp
@@ -1,1 +1,48 @@
+#ifndef LISTPARENT_H_INCLUDED
+#define LISTPARENT_H_INCLUDED
+#include <iostream>
+#define next(P) P->next
+#define info(P) P->info
+#define firstParent(L) L.firstParent
+
+//Single linked list
+
+using namespace std;
+
+struct infoParent
+{
+    int idPenyewa;
+    string alamatPenyewa;
+    string namaPenyewa;
+    int umurPenyewa;
+};
+
+typedef struct elmlistParent *Parent;
+
+struct elmlistChild
+{
+    infoParent info;
+    addressParent next;
+};
+
+struct listParent
+{
+    addressParent firstParent;
+};
+
+void createListParent(listParent &L);
+addressChild allocateParent(infoParent x);
+void insertFirstParent(listParent &L, addressParent P);
+void insertAfterParent(listParent &L, addressParent prec, addressParent P);
+void insertLastParent(listParent &L, addressParent P);
+void deleteFirstParent(listParent &L, addressParent &P);
+void deleteAfterParent(listParent &L, addressParent prec, addressParent &P);
+void deleteLastParent(listParent &L, addressParent &P);
+void printinfoParent(listParent L);
+addressParent searchElmParent(listParent &L, string ID);
+
+
+
+
+#endif // LISTPARENT_H_INCLUDED
 
