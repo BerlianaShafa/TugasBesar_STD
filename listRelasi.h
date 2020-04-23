fb2ff5acc4dd72cef9e2515a2b158f915c70272c
michellecfb <michellecfb@users.noreply.github.com>
2020-04-23 12:17:15 +0700

    Revert "updt listRelasi.h dan listRelasi.cpp"
    This reverts commit 9b80f03a55facfe45941083c1a0f4b153b0b12c0.

--- listRelasi.h
+++ listRelasi.h
@@ -1,48 +1,45 @@
 #ifndef LISTRELASI_H_INCLUDED
 #define LISTRELASI_H_INCLUDED
-#include "listChild.h"
-#include "listParent.h"
 #include <iostream>
-
 #define next(P) P->next
-#define next(prec) prec->next
-#define prev(P) P->prev
-#define first(L) L.first
-#define last(L) L.last
-#define child(P) P->child
-#define parent(P) P->parent
+#define info(P) P->info
+#define firstRelasi(L) L.firstRelasi
 
 //Single linked list
 
 using namespace std;
 
+struct infoRelasi
+{
+    int idPenyewa;
+    string alamatPenyewa;
+    string namaPenyewa;
+    int umurPenyewa;
+};
+
 typedef struct elmlistRelasi *addressRelasi;
 
 struct elmlistRelasi
 {
+    infoRelasi info;
     addressRelasi next;
-    addressRelasi prev;
-    addressChild child;
-    addressParent parent;
 };
 
 struct listRelasi
 {
-    addressRelasi first;
-    addressRelasi last;
+    addressRelasi firstRelasi;
 };
 
-void createList(listRelasi &L);
-addressRelasi allocateRelasi(addressParent parent, addressChild child);
-void deallocateRelasi(addressRelasi &P);
-void insertFirst(listRelasi &L, addressRelasi P);
-void insertAfter(listRelasi &L, addressRelasi prec, addressRelasi P);
-void insertLast(listRelasi &L, addressRelasi P);
-void deleteFirst(listRelasi &L, addressRelasi &P);
-void deleteAfter(listRelasi &L, addressRelasi prec, addressRelasi &P);
-void deleteLast(listRelasi &L, addressRelasi &P);
-void printinfo(listRelasi L);
-addressRelasi searchElm(listRelasi &L, addressParent parent, addressChild child);
+void createListRelasi(listRelasi &L);
+addressRelasi allocateRelasi(infoRelasi x);
+void insertFirstRelasi(listRelasi &L, addressRelasi P);
+void insertAfterRelasi(listRelasi &L, addressRelasi prec, addressRelasi P);
+void insertLastRelasi(listRelasi &L, addressRelasi P);
+void deleteFirstRelasi(listRelasi &L, addressRelasi &P);
+void deleteAfterRelasi(listRelasi &L, addressRelasi prec, addressRelasi &P);
+void deleteLastRelasi(listRelasi &L, addressRelasi &P);
+void printinfoRelasi(listRelasi L);
+addressRelasi searchElmRelasi(listRelasi &L, string ID);
 
 
 
